312c0d82617f01a1f60328a4c8656f92444ff722
Jennifer Browning <browningjen@yahoo.com>
2018-10-26 12:01:41 -0400

    first attempt

---
+++ main.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const fs = require("fs");
+const app = express()
+const categories = require("./categories.json")
+app.use(express.json())
+
+app.get("/api/category/:id", (req, res) => {
+    console.log('Woop, woop')
+    const requestedCategory = categories.find(category => category.id == req.params.id)
+    console.log(requestedCategory)
+    res.send(requestedCategory)
+})
+
+app.listen(3000, () => console.log("Hello World"))
